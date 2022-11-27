6c04b7004ac66307e7cb76d48bed4987c2c360eb
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 18:20:28 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interschimbam doua numere intregi
+            int x, y, aux;
+            Console.WriteLine("Introduceti doua numere intregi");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            aux = x;
+            x = y;
+            y = aux;
+            Console.WriteLine("Valori interschimbate, noul x este " + x + " iar y este " + y);
+        }
+    }
+}
